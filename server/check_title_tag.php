26968ff922a66fb79be114bab58b06d81c5654df
doctorjnupe <jhiggs@uab.edu>
2017-08-03 17:07:52 -0700

    Added missing variable URL.

--- server/check_title_tag.php
+++ server/check_title_tag.php
@@ -1,6 +1,7 @@
 <?php
 
   if(!empty($_POST['url_name'])){
+    $url = $_POST['url_name'];
     function getTitle($url) {
       $data = file_get_contents($url);
       $title = preg_match('/<title[^>]*>(.*?)<\/title>/ims', $data, $matches) ? $matches[1] : null;
